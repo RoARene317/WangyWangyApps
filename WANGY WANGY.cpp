fd36e3e88edf5298d0ca2d5be9c33a9ccc07236d
RoARene317 <herendra.kurniawan@gmail.com>
2021-01-17 20:56:58 +0700

    Initial Upload

---
+++ WANGY WANGY.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define ll long long int
+#define ull unsigned long long int
+#define printcase printf("Case #%d: ",i+1)
+
+int main(){
+char str[255];
+printf("Masukan nama 2D chara kalian: ");
+scanf("%[^\n]",&str);
+int n;
+printf("Pilih jenis yang di inginkan: ");
+scanf("%d",&n);
+	if (n == 1)
+		printf("watashi mencintai %s  dengan tulus dan penuh kasih sayang, watashi tidak tahan dengan hinaan kalian yg kalian berikan terhadap %s . %s  selalu menangis dikamarnya setiap malam karena hinaan kalian. \"shine, shine, shine\" hanya kata itulah yang ada dipikiran watashi tpi watashi hanya manusia lemah yang tak berdaya jika dikroyok banyak orang. Suatu saat kemudian ada orang biadab memfitnah %s  dengan membuat video skandal lalu menyebarkannya di website pornografi. Amarah dan aura merah menyelimuti watashi tanpa disadari darah menetes dari mata watashi secepat kilat watashi menengok cermin lalu watashi melihat rambut watashi belahan menjadi putih lalu ada kagune di punggung watashi lalu sosok kaneki muncul dari dalam cermin tanpa berkata apapun dia memberikan maskernya dan langsung pergi melompati jendela. Watashi langung mencuci masker itu karena bau tengik mulut kaneki membekas di masker itu. Perut Watashi tiba tiba merasa lapar, watashi mencoba indomie buatan %s  lalu watashi muntah muntah :confounded:, lalu terlintas dipikiran watashi jadi berita itu benar ! ghoul memang harus memakan manusia karena watashi masih mempunya jiwa manusia akhirnya watashi memakan tangan sendiri. Meskipun tidak menghilangkan rasa lapar setidaknya ini cukup untuk berdiri lalu memangsa siapapun yang menghina istri watashi. no mercy no cry u must die, watashi tak segan segan membunuh kalian jika kalian menghina %s .\n",str,str,str,str,str,str);
+	else if(n == 2)
+		printf("%s  %s  %s   HEART	 HEART	HEART	 WANGI WANGI WANGI WANGI HU HA HU HA HU HA, aaaah baunya rambut %s  wangi aku mau nyiumin aroma wanginya %s  AAAAAAAAH ~ Rambutnya.... aaah rambutnya juga pengen aku elus-elus ~~~~ AAAAAH %s  keluar pertama kali di anime juga manis HEART	 HEART	 HEART	 banget AAAAAAAAH %s  AAAAA LUCCUUUUUUUUUUUUUUU............%s  AAAAAAAAAAAAAAAAAAAAGH HEART	 HEART	 HEART	apa ? %s  itu gak nyata ? Cuma HALU katamu ? nggak, ngak ngak ngak ngak NGAAAAAAAAK GUA GAK PERCAYA ITU DIA NYATA NGAAAAAAAAAAAAAAAAAK PEDULI BANGSAAAAAT !! GUA GAK PEDULI SAMA KENYATAAN POKOKNYA GAK PEDULI. HEART	 HEART	 HEART	 %s  gw ...%s  di laptop ngeliatin gw, %s  .. kamu percaya sama aku ? aaaaaaaaaaah syukur %s  aku gak mau merelakan %s  aaaaaah HEART	 HEART	 HEART	 YEAAAAAAAAAAAH GUA MASIH PUNYA %s  SENDIRI PUN NGGAK SAMA AAAAAAAAAAAAAAH.",str,str,str,str,str,str,str,str,str,str,str,str,str,str,str);
+	else if (n == 3)
+		printf("GW BENAR-BENAR PENGEN JILATI KAKI %s . GW PENGEN BANGET MENJILAT SETIAP BAGIAN KAKINYA SAMPAI AIR LIUR GW BERCUCURAN KAYAK AIR KERINGAT LALU NGENOD DENGANNYA SETIAP HARI SAMPAI TUBUH KITA MATI RASA YA TÜHAN. GW INGIN MEMBUAT ANAK-ANAK DENGAN %s  SEBANYAK SATU TÌM SEPAK BOLA DAN MEMBUAT SATU TIM SEPAK BOLA LAINNYA UNTUK MELAWAN ANAK-ANAK TIM SEPAK BOLA PERTAMA GW YANG GW BUAT SAMA %s . GW PENGEN MASUK KE SETIAP LUBANG TUBUHNYA, MAU ITU LUBANG HIDUNG, LUBANG TELINGA, RONGGA MATA MAUPUN PUSAR, KECUALI PORI-PORI KULIT. KEMUDIAN GW AKAN MENJADIKANNYA MANUSIA YANG TIDAK BISA HIDUP KALO TIDAK GW KENTOG SETIAP HARI. DAN KALAU GUA DISEPONG GUA RELA KONTL GUA PUTUS.",str,str,str);
+	else if (n == 4)
+		printf("Buruan, panggil gue SIMP, ato BAPERAN. ini MURNI PERASAAN GUE. Gue pengen genjot bareng %s . Ini seriusan, suaranya yang imut, mukanya yang cantik, apalagi badannya yang aduhai ningkatin gairah gue buat genjot %s . Setiap lapisan kulitnya pengen gue jilat. Saat gue mau crot, gue bakal moncrot sepenuh hati, bisa di perut, muka, badan, teteknya, sampai lubang burit pun bakal gue crot sampai puncak klimaks. Gue bakal meluk dia abis gue moncrot, lalu nanya gimana kabarnya, ngrasain enggas bareng saat telanjang. Dia bakal bilang kalau genjotan gue mantep dan nyatain perasaannya ke gue, bilang kalo dia cinta ama gue. Gue bakal bilang balik seberapa gue cinta ama dia, dan dia bakal kecup gue di pipi. Terus kita ganti pakaian dan ngabisin waktu nonton film, sambil pelukan ama makan hidangan favorit. Gue mau %s  jadi pacar, pasangan, istri, dan idup gue. Gue cinta dia dan ingin dia jadi bagian tubuh gue. Lo kira ini copypasta? Kagak cok. Gue ngetik tiap kata nyatain prasaan gue. Setiap kali elo nanya dia siapa, denger ini baik-baik : DIA ISTRI GUE. Gue sayang %s , dan INI MURNI PIKIRAN DAN PERASAAN GUE.",str,str,str,str);
+	else
+		printf("Ayo Ke Dokter!!");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("Tekan Ctrl + C untuk mengakhiri program...");
+sleep (3600);
+return 0;
+}
